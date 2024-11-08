a0d2adae256efd0ba8bc7b588667f0b25d884cdd
Nishtman_Coding <Farid.physic2024@gmail.com>
2024-11-08 09:52:30 -0800

    Project and latest article moved to view component

---
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            var Articles = new List<Article> {
+
+                new Article(id: 1, title:"آموزش asp.net core" , description:" asp.net آموزش فوق العاده " , image:"blog-post-thumb-card-3.jpg"),
+                new Article(id: 2, title:"آموزش گیت و گیت هاب" , description:"گیت و گیت هاب یکی از خفن ترین برنامه های " , image:"blog-post-thumb-card-4.jpg"),
+                new Article(id: 3, title: "آموزش برنامه نویسی" , description:"آموزش برنامه نویسی بک اند با سی شارپ" , image:"blog-post-thumb-card-5.jpg"),
+                new Article(id: 4, title:"آموزش هوش مصنوعی" , description:"هوش مصنوعی را با قدرت شروع کنید" , image:"blog-post-thumb-card-6.jpg")
+
+         };
+
+            
+            return View("_LatestArticles", Articles );
+        }
+
+    }
+}
---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponents.cs
@@ -0,0 +1,28 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke() // This method return that view component where we create 
+        {
+
+            var projects = new List<Project>
+            {
+                new Project(id:1 , name:"تاکسی", description:"تاکسی برای سفرهای درون شهری", image:"project-1.jpg", client:"Atria"),
+                new Project(id:2 , name:"زودفود", description:"درخواست انلاین غذا",image:"project-2.jpg" , client:"ZoodFood"),
+                new Project(id:3 , name:"مدارس", description:"سیستم یکپارچه سرار ایران", image:"project-3.jpg" ,  client:"Monop"),
+                new Project(id:4 , name:"فضاپیما", description:"شرکت مدیرت فضاپیما", image:"project-4.jpg"  , client:"Nasa"),
+
+
+
+            };
+
+            return View("_Projects" , projects ); //we return _Projects view component
+
+        }
+
+
+    }
+}
